50ab2936ccab11b3a7027f37fba210438a7de245
lutfiqaraman <lutfiqaraman@gmail.com>
2018-04-29 20:59:07 +0300

    Done

--- SmallestInteger/Program.cs
+++ SmallestInteger/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SmallestInteger
 {
@@ -10,6 +8,27 @@ namespace SmallestInteger
     {
         static void Main(string[] args)
         {
+            List<int> numbers = new List<int>();
+            int randomNumber, length, smallestNumber, highestNumber;
+
+            Random rnd = new Random();
+            length = rnd.Next(1, 10);
+
+            for (int i = 0; i < length; i++)
+            {
+                randomNumber = rnd.Next(-100, 100);
+                numbers.Add(randomNumber);
+                Console.Write("\t" + numbers[i]);
+            }
+
+            smallestNumber = numbers.Min();
+            highestNumber  = numbers.Max();
+
+            Console.WriteLine();
+            Console.WriteLine("Smallest number : {0}", smallestNumber);
+            Console.WriteLine("Highest number  : {0}", highestNumber);
+
+            Console.ReadKey();
         }
     }
 }
